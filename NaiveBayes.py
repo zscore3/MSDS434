7d26ebc45223af10485d51f57faabe899788ac39
zscore3 <60632046+zscore3@users.noreply.github.com>
2025-05-26 16:42:42 -0400

    Create NaiveBayes.py

---
+++ NaiveBayes.py
@@ -0,0 +1,43 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import GaussianNB
+from sklearn.metrics import (
+    accuracy_score,
+    confusion_matrix,
+    ConfusionMatrixDisplay,
+    f1_score,
+    classification_report,
+)
+
+
+#Data Loading
+df = pd.read_csv('loan_data.csv')
+df.head()
+
+pre_df = pd.get_dummies(df,columns=['purpose'],drop_first=True)
+pre_df.head()
+
+
+#Data Processing 
+X = pre_df.drop('not.fully.paid', axis=1)
+y = pre_df['not.fully.paid']
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.33, random_state=125
+)
+
+
+#Heading 3
+model = GaussianNB()
+
+model.fit(X_train, y_train);
+
+
+#Heading 4
+y_pred = model.predict(X_test)
+
+accuracy = accuracy_score(y_pred, y_test)
+f1 = f1_score(y_pred, y_test, average="weighted")
+
+print("Accuracy:", accuracy)
+print("F1 Score:", f1)
